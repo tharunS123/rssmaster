420ea7099aba3444e7dbe943a0bcad8406068051
tharunS123 <tharunsenthilkumar308@gmail.com>
2025-10-03 14:44:08 -0400

    Initial Commit

---
+++ rssmaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rssmaster
+//
+//  Created by Tharun Senthilkumar on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rssmaster/rssmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  rssmasterApp.swift
+//  rssmaster
+//
+//  Created by Tharun Senthilkumar on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct rssmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
